b155588cbdf7a42f0b2d11e7003bad50180b0260
VIPon4ik <umniyd@gmail.com>
2023-10-10 18:07:15 +0300

    Complete task with infinite scroll

--- src/index.js
+++ src/index.js
@@ -1,62 +1,66 @@
 'use strict';
 
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from "axios";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const apiKey = '19760378-d2f1c7488e9d6752a9d7092b3';
 const axiosOptions = {
-    key: apiKey,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
+  key: apiKey,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
 };
+
+const throttle = require('lodash.throttle');
 
 const formElem = document.querySelector('form');
 const galleryElem = document.querySelector('.gallery');
-const nextPageButt = document.querySelector('.load-more');
 const inputElem = document.querySelector('input');
 
 let inputValue = '';
 let page = 1;
+let hasEventListener = false;
 
 const lightbox = new SimpleLightbox('.photo-card a');
 
 inputElem.addEventListener('input', () => {
-    inputValue = inputElem.value;
-    nextPageButt.style.display = 'none';
+  inputValue = inputElem.value;
+  window.removeEventListener('scroll', throttleFunction);
+  hasEventListener = false;
 });
 
-formElem.addEventListener('submit', (event) => {
-    event.preventDefault();
+formElem.addEventListener('submit', event => {
+  event.preventDefault();
 
-    page = 1;
-    galleryElem.innerHTML = '';
+  if (!hasEventListener) {
+    window.addEventListener('scroll', throttleFunction);
+    hasEventListener = true;
+  };
 
-    generatingImages(inputValue, page);
-});
+  page = 1;
+  galleryElem.innerHTML = '';
 
-nextPageButt.addEventListener('click', (event) => {
-    page++;
-
-    generatingImages(inputValue, page);
+  generatingImages(inputValue, page);
 });
 
 async function gettingApiImages(query, page) {
-    try {
-        const response = await axios.get('https://pixabay.com/api/', { params: { ...axiosOptions, q: query, page: page }});
-        const data = await response.data;
-        return data;
-    } catch (error) {
-        Notiflix.Notify.failure('Sorry, there are problems with API.');
-    };
-};
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: { ...axiosOptions, q: query, page: page },
+    });
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    Notiflix.Notify.failure('Sorry, there are problems with API.');
+  }
+}
 
 function markupImages(images) {
-    images.forEach((img) => {
-        const imgMarkup = `
+  images.forEach(img => {
+    const imgMarkup = `
             <div class="photo-card">
                 <a href="${img.largeImageURL}">
                     <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
@@ -80,42 +84,41 @@ function markupImages(images) {
                     </p>
                 </div>
             </div>
-        `
-        galleryElem.insertAdjacentHTML('beforeend', imgMarkup);
-    });
+        `;
+    galleryElem.insertAdjacentHTML('beforeend', imgMarkup);
+  });
+}
+
+function handleScroll() {
+    if (window.innerHeight + window.scrollY + 10 >= document.body.offsetHeight) {
+        page++;
+        generatingImages(inputValue, page);
+    }
 };
 
-function smoothScroll() {
-    if (page === 1) {
-        return;
-    };
-
-    const galleryChildsList = document.querySelectorAll('.photo-card');
-    const { height: cardHeight } = galleryChildsList[galleryChildsList.length - 1].getBoundingClientRect();
-
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    });
-};
+const throttleFunction = throttle((handleScroll), 1000);
 
 async function generatingImages(q, page) {
-    const data = await gettingApiImages(q, page);
-    if (!data.hits.length) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        galleryElem.innerHTML = '';
-        return;
-    };
+  const data = await gettingApiImages(q, page);
+  if (!data.hits.length) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    window.removeEventListener('scroll', throttleFunction);
+    hasEventListener = false;
+    galleryElem.innerHTML = '';
+    return;
+  }
 
-    markupImages(data.hits);
-    lightbox.refresh();
-    nextPageButt.style.display = 'block';
+  markupImages(data.hits);
+  lightbox.refresh();
 
-    smoothScroll();
-
-    if (data.totalHits === galleryElem.childElementCount || galleryElem.childElementCount >= 500) {
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-        nextPageButt.style.display = 'none';
-        return;
-    };
+  if (data.totalHits === galleryElem.childElementCount || galleryElem.childElementCount >= 500) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    window.removeEventListener('scroll', throttleFunction);
+    hasEventListener = false;
+    return;
+  }
 }
